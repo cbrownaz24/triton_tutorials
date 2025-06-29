b9f00907f31d044e21dd5417cb72da29a15a2bb6
Connor Brown <connorbrown059@gmail.com>
2025-06-29 21:08:51 +0000

    Added basic data parallel kernel

--- streamk-matmul.py
+++ streamk-matmul.py
@@ -3,37 +3,39 @@ import triton.language as tl
 
 @triton.jit
 def streamk_kernel(
-    # Pointers to matrices
-    a_ptr, b_ptr, c_ptr,
-    # Matrix dimensions
-    M, N, K,
-    # The stride variables represent how much to increase the ptr by when moving by 1
-    # element in a particular dimension. E.g. `stride_am` is how much to increase `a_ptr`
-    # by to get the element one row down (A has M rows).
-    stride_am, stride_ak, #
-    stride_bk, stride_bn, #
-    stride_cm, stride_cn, #
-    # Meta-parameters
-    BLOCK_SIZE_M: tl.constexpr, BLOCK_SIZE_N: tl.constexpr, BLOCK_SIZE_K: tl.constexpr, #
-    t0: tl.constexpr, # tile index offset (phase start)
-    units: tl.constexpr, # number of units of work for stream-k
+        # Pointers to matrices
+        a_ptr, b_ptr, c_ptr,
+        # Matrix dimensions
+        M, N, K,
+        # The stride variables represent how much to increase the ptr by when moving by 1
+        # element in a particular dimension. E.g. `stride_am` is how much to increase `a_ptr`
+        # by to get the element one row down (A has M rows).
+        stride_am, stride_ak, 
+        stride_bk, stride_bn,
+        stride_cm, stride_cn,
+        # The work unit for Stream-K to begin working on.
+        start_unit,
+        # Total number of units for Stream-K to work on.
+        tot_units,
+        # Meta-parameters
+        BLOCK_SIZE_M: tl.constexpr, 
+        BLOCK_SIZE_N: tl.constexpr, 
+        BLOCK_SIZE_K: tl.constexpr
 ):
-    # Program and grid identification
+    # Map program ids `pid` to the block of C it should compute.
     pid = tl.program_id(axis=0)
-    num_pids = tl.num_programs(axis=0)
+    grid_n = tl.cdiv(N, BLOCK_SIZE_N)
 
-    # Tile grid dimensions and sub-dot counts
-    grid_n = tl.cdiv(N, BLOCK_SIZE_N) # tiles along N
+    n_pids = tl.num_programs(axis=0)
+
     n_sub = tl.cdiv(K, BLOCK_SIZE_K) # number of sub-dot blocks per tile
 
-    # Loop over the program's assigned work units
-    for u in range(pid, units, num_pids):
-        tile = t0 + u // n_sub # index of the tile
-        k_loc = u % n_sub # sub-dot index
-
-        # Decode tile coordinates
-        pid_m = tile // grid_n
-        pid_n = tile % grid_n
+    for unit_idx in range(start_unit, last_unit, n_pids):
+        # Tile and sub-dot block indexing
+        tile = unit // n_sub # index of the tile
+        tile_m = tile // grid_n
+        tile_n = tile % grid_n
+        k_loc = unit % n_sub # sub-dot index
 
         # Determine effective k-block size for final sub-dot
         k0 = k_loc * BLOCK_SIZE_K
@@ -47,8 +49,8 @@ def streamk_kernel(
 
         # Pointer offsets
         offs_k = (k0 + tl.arange(0, BLOCK_SIZE_K)) % K
-        offs_am = (pid_m * BLOCK_SIZE_M + tl.arange(0, BLOCK_SIZE_M)) % M
-        offs_bn (pid_n * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)) % N
+        offs_am = (tile_m * BLOCK_SIZE_M + tl.arange(0, BLOCK_SIZE_M)) % M
+        offs_bn (tile_n * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)) % N
         a_ptrs = a_ptr + (offs_am[:, None] * stride_am + offs_k[None, :] * stride_ak)
         b_ptrs = b_ptr + (offs_k[:, None] * stride_bk + offs_bn[None, :] * stride_bn)
 
@@ -68,8 +70,63 @@ def streamk_kernel(
         P = tl.dot(A_blk, B_blk)    # [bM, bN]
 
         # Accumulate and write back the output block.
-        offs_cm = pid_m * BLOCK_SIZE_M + tl.arange(0, BLOCK_SIZE_M)
-        offs_cn = pid_n * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)
+        offs_cm = tile_m * BLOCK_SIZE_M + tl.arange(0, BLOCK_SIZE_M)
+        offs_cn = tile_n * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)
         c_ptrs = c_ptr + stride_cm * offs_cm[:, None] + stride_cn * offs_cn[None, :]
         c_mask = (offs_cm[:, None] < M) & (offs_cn[None, :] < N)
         tl.atomic_add(c_ptrs, P, mask=c_mask)
+
+@triton.jit
+def matmul_kernel(
+        # Pointers to matrices
+        a_ptr, b_ptr, c_ptr,
+        # Matrix dimensions
+        M, N, K,
+        # The stride variables represent how much to increase the ptr by when moving by 1
+        # element in a particular dimension. E.g. `stride_am` is how much to increase `a_ptr`
+        # by to get the element one row down (A has M rows).
+        stride_am, stride_ak,
+        stride_bk, stride_bn,
+        stride_cm, stride_cn,
+        # Meta-parameters
+        BLOCK_SIZE_M: tl.constexpr, 
+        BLOCK_SIZE_N: tl.constexpr, 
+        BLOCK_SIZE_K: tl.constexpr,
+):
+    # Map program ids `pid` to the block of C it should compute.
+    pid = tl.program_id(axis=0)
+    grid_n = tl.cdiv(N, BLOCK_SIZE_N)
+    pid_m = pid // grid_n
+    pid_n = pid % grid_n
+
+    # Create pointers for the first blocks of A and B.
+    # We will advance this pointer as we move in the K direction
+    # and accumulate
+    offs_am = (pid_m * BLOCK_SIZE_M + tl.arange(0, BLOCK_SIZE_M)) % M
+    offs_bn = (pid_n * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)) % N
+    offs_k = tl.arange(0, BLOCK_SIZE_K)
+    a_ptrs = a_ptr + (offs_am[:, None] * stride_am + offs_k[None, :] * stride_ak)
+    b_ptrs = b_ptr + (offs_k[:, None] * stride_bk + offs_bn[None, :] * stride_bn)
+
+    # Iterate to compute a block of the C matrix.
+    # We accumulate into a `[BLOCK_SIZE_M, BLOCK_SIZE_N]` block
+    # of fp32 values for higher accuracy.
+    # `accumulator` will be converted back to fp16 after the loop.
+    accumulator = tl.zeros((BLOCK_SIZE_M, BLOCK_SIZE_N), dtype=tl.float32)
+    for k in range(0, tl.cdiv(K, BLOCK_SIZE_K)):
+        # Load the next block of A and B, generate a mask by checking the K dimension.
+        # If it is out of bounds, set it to 0.
+        a = tl.load(a_ptrs, mask=offs_k[None, :] < K - k * BLOCK_SIZE_K, other=0.0)
+        b = tl.load(b_ptrs, mask=offs_k[:, None] < K - k * BLOCK_SIZE_K, other=0.0)
+        # We accumulate along the K dimension.
+        accumulator = tl.dot(a, b, accumulator)
+        # Advance the ptrs to the next K block.
+        a_ptrs += BLOCK_SIZE_K * stride_ak
+        b_ptrs += BLOCK_SIZE_K * stride_bk
+
+    # Write back the block of the output matrix C with masks.
+    offs_cm = pid_m * BLOCK_SIZE_M + tl.arange(0, BLOCK_SIZE_M)
+    offs_cn = pid_n * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)
+    c_ptrs = c_ptr + stride_cm * offs_cm[:, None] + stride_cn * offs_cn[None, :]
+    c_mask = (offs_cm[:, None] < M) & (offs_cn[None, :] < N)
+    tl.store(c_ptrs, c, mask=c_mask)
