375cc5bb170b478454d1ad7db71402fe0ab4ea3d
Connor Brown <connorbrown059@gmail.com>
2025-06-29 21:46:33 +0000

    added kernel launcher

--- streamk-matmul.py
+++ streamk-matmul.py
@@ -1,6 +1,8 @@
 import triton
 import triton.language as tl
 
+import torch
+
 @triton.jit
 def streamk_kernel(
         # Pointers to matrices
@@ -13,10 +15,10 @@ def streamk_kernel(
         stride_am, stride_ak, 
         stride_bk, stride_bn,
         stride_cm, stride_cn,
-        # The work unit for Stream-K to begin working on.
-        start_unit,
-        # Total number of units for Stream-K to work on.
-        tot_units,
+        # The work unit ranges and distribution along k-dimension.
+        start_unit, # First unit for Stream-K to work on.
+        end_unit, # Last unit for Stream-K to work on.
+        n_sub, # Number of sub-dot blocks per tile.
         # Meta-parameters
         BLOCK_SIZE_M: tl.constexpr, 
         BLOCK_SIZE_N: tl.constexpr, 
@@ -28,9 +30,7 @@ def streamk_kernel(
 
     n_pids = tl.num_programs(axis=0)
 
-    n_sub = tl.cdiv(K, BLOCK_SIZE_K) # number of sub-dot blocks per tile
-
-    for unit_idx in range(start_unit, last_unit, n_pids):
+    for unit_idx in range(start_unit, end_unit, n_pids):
         # Tile and sub-dot block indexing
         tile = unit // n_sub # index of the tile
         tile_m = tile // grid_n
@@ -130,3 +130,55 @@ def matmul_kernel(
     c_ptrs = c_ptr + stride_cm * offs_cm[:, None] + stride_cn * offs_cn[None, :]
     c_mask = (offs_cm[:, None] < M) & (offs_cn[None, :] < N)
     tl.store(c_ptrs, c, mask=c_mask)
+
+def p_matmul(a, b):
+    # Check constraints
+    assert a.shape[1] == b.shape[0], "Incompatible dimensions"
+    assert a.is_contiguous(), "Matrix A must be contiguous"
+
+    device = torch.cuda.current_device()
+    props = torch.cuda.get_device_properties(device)
+    n_sms = props.multi_processor_count
+
+    print(f"GPU has {num_sms} SMs")
+
+    M, K = a.shape
+    K, N = b.shape
+
+    # Compute number of tiles for Stream-K to handle
+    n_tiles = lambda META: (triton.cdiv(M, META['BLOCK_SIZE_M']) * triton.cdiv(N, META['BLOCK_SIZE_N']))
+    n_sub = lambda META: triton.cdiv(K, META['BLOCK_SIZE_K'])
+    n_rem_tiles = n_tiles % n_sms
+    do_add_full_wave = triton.cdiv(n_tiles, n_sms) > 1 and (not n_rem_tiles == 0)
+    n_rem_tiles = triton.where(do_add_full_wave, n_rem_tiles + n_sms, n_rem_tiles)
+    n_dataparallel_tiles = n_tiles - n_rem_tiles
+
+    # Allocates output.
+    c = torch.empty((M, N), device=a.device, dtype=torch.float16)
+
+    # 1D launch kernel for data-parallel phase
+    matmul_kernel[n_dataparallel_tiles](
+        a, b, c,  #
+        M, N, K,  #
+        a.stride(0), a.stride(1),  #
+        b.stride(0), b.stride(1),  #
+        c.stride(0), c.stride(1),  #
+    )
+    
+    streamk_start_unit = n_dataparallel_tiles * n_sub - 1
+    n_streamk_units = n_rem_tiles * n_sub
+    streamk_end_unit = streamk_start_unit + n_streamk_units
+
+    # 1D launch kernel for Stream-K phase
+    streamk_kernel[n_sms](
+        a, b, c,  #
+        M, N, K,  #
+        a.stride(0), a.stride(1),  #
+        b.stride(0), b.stride(1),  #
+        c.stride(0), c.stride(1),  #
+        streamk_start_unit, #
+        streamk_end_unit, #
+        n_sub, #
+    )
+
+    return c
